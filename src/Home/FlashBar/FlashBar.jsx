af8876d655a2173bac1f6df9bfaed2fe999d4108
manopriya-coder <manopriya21022005@gmail.com>
2025-08-22 18:40:52 +0530

    phone view

--- src/Home/FlashBar/FlashBar.jsx
+++ src/Home/FlashBar/FlashBar.jsx
@@ -4,8 +4,8 @@ import "../FlashBar/FlashBar.css"
 const FlashBar = () => {
   return (
     <div className='mainBar'>
-        <p>UI/UX Designer</p>
-        <i class="ri-star-fill"></i>
+        <p className='none'>UI/UX Designer</p>
+        <i class="ri-star-fill none"></i>
         <p>UI/UX Designer</p>
         <i class="ri-star-fill"></i>
         <p>UI/UX Designer</p>
